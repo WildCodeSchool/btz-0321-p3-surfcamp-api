c9a32faa6d81f8ad28f29b68a7bcc06b001454d0
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-09 15:13:24 +0200

    headers-added

--- src/api/users/getAll.ts
+++ src/api/users/getAll.ts
@@ -4,6 +4,11 @@ import UserHandlers from "./interfaces";
 
 const getAll: UserHandlers["getAll"] = async (req, res) => {
   const users = await prisma.user.findMany();
+  res.setHeader('X-Total-Count',200)
+  res.set({
+    'X-Total-Count': '100',
+    'Access-Control-Expose-Headers': 'X-Total-Count'
+  })
   res.status(200).json(users);
 };
 
