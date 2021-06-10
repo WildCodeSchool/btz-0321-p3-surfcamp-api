32e744dd4d404700e82e513cdce8ccadf7b9001b
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-10 06:59:30 +0200

    seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,7 +5,7 @@ import createAddress from "./createAdressSeed";
 const seed: any = async () => {
   createProperty(40);
   createUser(40);
-  return "40 adresses properties and users was created";
+  return "40 adresses properties and users were created";
 };
 
 export default seed;
