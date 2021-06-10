1c588e4bcf02c66b719ac104e76c6a509021f7a0
Julien <70638063+JIdayyy@users.noreply.github.com>
2021-06-10 11:45:32 +0200

    Update post.ts

--- src/api/properties/post.ts
+++ src/api/properties/post.ts
@@ -4,15 +4,6 @@ import PropertyHandlers from "./interfaces";
 
 const post: PropertyHandlers["post"] = async (req, res) => {
 
-  const {
-    name,
-    priceByNight,
-    type,
-    description,
-    address,
-    status,
-    rooms,
-  } = req.body;
 
   const { name, priceByNight, type, description, status, addressId } = req.body;
 
