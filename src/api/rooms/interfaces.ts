a1b924045b25e0f5bb0dee6a7a412533f0d099d3
Thiez-64 <ma.ojeanson@gmail.com>
2021-06-10 09:57:41 +0200

    create ressource room

---
+++ src/api/rooms/interfaces.ts
@@ -0,0 +1,32 @@
+import { RequestHandler } from "express";
+import { Property, Room } from "@prisma/client";
+
+interface ReqBodyRoomPost {
+  name: string;
+  description: string;
+  capacity: number;
+  priceByNight: number;
+  propertyId: string;
+  Property?: Property;
+}
+
+interface ReqBodyRoomPut {
+  name: string;
+  description: string;
+  capacity: number;
+  priceByNight: number;
+  propertyId: string;
+  Property?: Property;
+}
+
+interface Params {
+  id: string;
+}
+
+export default interface RoomHandlers {
+  getAll: RequestHandler<null, Room[], null>;
+  getOne: RequestHandler<Params, Room, null>;
+  post: RequestHandler<null, ReqBodyRoomPost>;
+  put: RequestHandler<Params, null, ReqBodyRoomPut>;
+  delete: RequestHandler<Params, null, null>;
+}
