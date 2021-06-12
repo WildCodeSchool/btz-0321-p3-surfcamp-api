2ed5c6cca0d5162f342b71c618e5278eac142a93
Julien <70638063+JIdayyy@users.noreply.github.com>
2021-06-12 11:52:31 +0200

    Update getAll.ts

--- src/api/rooms/getAll.ts
+++ src/api/rooms/getAll.ts
@@ -4,6 +4,11 @@ import RoomHandlers from "./interfaces";
 
 const getAll: RoomHandlers["getAll"] = async (req, res) => {
   const rooms = await prisma.room.findMany();
+   res.setHeader("X-Total-Count", 200);
+  res.set({
+    "X-Total-Count": "100",
+    "Access-Control-Expose-Headers": "X-Total-Count",
+  });
   res.status(200).json(rooms);
 };
 
