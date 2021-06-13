1290c3192412ba09b51cdb549c393e316a1554c4
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-13 09:05:18 +0200

    x-headers set on ressource.length  on all getAll routes for ReactAdmin pagination

--- src/api/rooms/getAll.ts
+++ src/api/rooms/getAll.ts
@@ -4,9 +4,9 @@ import RoomHandlers from "./interfaces";
 
 const getAll: RoomHandlers["getAll"] = async (req, res) => {
   const rooms = await prisma.room.findMany();
-  res.setHeader("X-Total-Count", 200);
+  res.setHeader("X-Total-Count", rooms.length);
   res.set({
-    "X-Total-Count": "100",
+    "X-Total-Count": rooms.length,
     "Access-Control-Expose-Headers": "X-Total-Count",
   });
   res.status(200).json(rooms);
--- src/api/users/getAll.ts
+++ src/api/users/getAll.ts
@@ -3,9 +3,9 @@ import UserHandlers from "./interfaces";
 
 const getAll: UserHandlers["getAll"] = async (req, res) => {
   const users = await prisma.user.findMany();
-  res.setHeader("X-Total-Count", 200);
+  res.setHeader("X-Total-Count", users.length);
   res.set({
-    "X-Total-Count": "100",
+    "X-Total-Count": users.length,
     "Access-Control-Expose-Headers": "X-Total-Count",
   });
   res.status(200).json(users);
