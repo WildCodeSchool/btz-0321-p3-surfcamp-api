506acffc6a73c88e38572b62db6d1c0299bb9c55
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-13 15:16:21 +0200

    removed unused import

--- src/middleware/postUserSchemaValidator.ts
+++ src/middleware/postUserSchemaValidator.ts
@@ -1,5 +1,5 @@
 import postUserSchema from "../JOI/users/postUserSchemaJOI";
-import { RequestHandler } from "express";
+
 export default function postUserSchemaValidator(req: any, res: any, next: any) {
   const result = postUserSchema.validate(req.body);
   const { value, error } = result;
