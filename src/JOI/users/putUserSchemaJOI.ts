aa90e32ea2509791c1ee2e0780b213020f2f5e7f
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-23 20:00:43 +0200

    fix put/post

--- src/JOI/users/putUserSchemaJOI.ts
+++ src/JOI/users/putUserSchemaJOI.ts
@@ -1,15 +1,20 @@
 import Joi from "joi";
 
 const putUserSchema = Joi.object().keys({
-  firstname: Joi.string().min(1).max(50),
-  lastname: Joi.string().min(1).max(50),
-  email: Joi.string().email(),
-  password: Joi.string().min(6).max(30),
-  picture: Joi.string(),
-  birthDate: Joi.date().iso(),
-  phoneNumber: Joi.string(),
-  role: Joi.string(),
-  isActive: Joi.boolean(),
+  firstname: Joi.string().alphanum().min(3).max(30).required(),
+  lastname: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email({ allowUnicode: false }).required(),
+  password: Joi.string()
+    .pattern(
+      new RegExp(
+        /^(?=.*[A-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`])\S{8,30}$/
+      )
+    )
+    .required(),
+  confirmPassword: Joi.ref("password"),
+  birthDate: Joi.date().iso().min("1-1-1900").max("now").required(),
+  phoneNumber: Joi.string().min(10).max(10).required(),
+  picture: Joi.string().uri().required(),
   addressId: Joi.string(),
   propertyId: Joi.string(),
 });
