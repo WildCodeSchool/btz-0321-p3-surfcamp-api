17b2a1b3a338899b6209a17574027dd0a82b4a06
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-24 16:44:04 +0200

    user requirements

--- src/JOI/users/postUserSchemaJOI.ts
+++ src/JOI/users/postUserSchemaJOI.ts
@@ -1,13 +1,13 @@
 import Joi from "joi";
 
 const postUserSchema = Joi.object().keys({
-  firstname: Joi.string().alphanum().min(3).max(30).required(),
-  lastname: Joi.string().alphanum().min(3).max(30).required(),
+  firstname: Joi.string().alphanum().min(3).max(30),
+  lastname: Joi.string().alphanum().min(3).max(30),
   email: Joi.string().email({ allowUnicode: false }).required(),
   password: Joi.string().required(),
   confirmPassword: Joi.ref("password"),
   birthDate: Joi.date().iso().min("1-1-1900").max("now"),
-  phoneNumber: Joi.string().required(),
+  phoneNumber: Joi.string(),
   picture: Joi.string().uri(),
   addressId: Joi.string(),
   address: Joi.any(),
