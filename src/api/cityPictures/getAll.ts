8669ef0ca30e1f909267dc7cb2eb378d1e79ea47
JIdayyy <julien_abbadie@hotmail.fr>
2021-06-26 23:29:56 +0200

    headers set length

--- src/api/cityPictures/getAll.ts
+++ src/api/cityPictures/getAll.ts
@@ -1,30 +1,23 @@
 import prisma from "../../../prisma/prismaClient";
-import parser from "../../../utils/parser"
+import parser from "../../../utils/parser";
 import CityPictureHandlers from "./interfaces";
 
 const getAll: CityPictureHandlers["getAll"] = async (req, res) => {
-  const { skip, take } = req.query
+  const { skip, take } = req.query;
 
-  const parsedSkip = parser(skip)
-  const parsedTake = parser(take)
-
+  const parsedSkip = parser(skip);
+  const parsedTake = parser(take);
 
   if (req.query) {
-    const cityPicture = await prisma.cityPicture.findMany(
-      {
-        skip: parsedSkip,
-        take: parsedTake
-      }
-    );
+    const cityPicture = await prisma.cityPicture.findMany({
+      skip: parsedSkip,
+      take: parsedTake,
+    });
+    res.setHeader("X-Total-Count", cityPicture.length);
     res.status(200).json(cityPicture);
   } else {
-
     const cityPicture = await prisma.cityPicture.findMany();
     res.setHeader("X-Total-Count", cityPicture.length);
-    res.set({
-      "X-Total-Count": cityPicture.length,
-      "Access-Control-Expose-Headers": "X-Total-Count",
-    });
     res.status(200).json(cityPicture);
   }
 };
--- src/api/countryPictures/getAll.ts
+++ src/api/countryPictures/getAll.ts
@@ -3,28 +3,21 @@ import parser from "../../../utils/parser";
 import CountryPictureHandlers from "./interfaces";
 
 const getAll: CountryPictureHandlers["getAll"] = async (req, res) => {
-  const { skip, take } = req.query
+  const { skip, take } = req.query;
 
-  const parsedSkip = parser(skip)
-  const parsedTake = parser(take)
-
+  const parsedSkip = parser(skip);
+  const parsedTake = parser(take);
 
   if (req.query) {
-    const countryPicture = await prisma.countryPicture.findMany(
-      {
-        skip: parsedSkip,
-        take: parsedTake
-      }
-    );
+    const countryPicture = await prisma.countryPicture.findMany({
+      skip: parsedSkip,
+      take: parsedTake,
+    });
+    res.setHeader("X-Total-Count", countryPicture.length);
     res.status(200).json(countryPicture);
   } else {
-
     const countryPicture = await prisma.countryPicture.findMany();
     res.setHeader("X-Total-Count", countryPicture.length);
-    res.set({
-      "X-Total-Count": countryPicture.length,
-      "Access-Control-Expose-Headers": "X-Total-Count",
-    });
     res.status(200).json(countryPicture);
   }
 };
