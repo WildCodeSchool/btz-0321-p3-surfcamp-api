70f2a83bcda3c71251b5aed5142351e3a99d729b
Mathias Ryckewaert <mathryckdel@free.fr>
2021-07-16 11:03:57 +0200

    lat and long are no more required

--- src/JOI/address/postAddressSchemaJOI.ts
+++ src/JOI/address/postAddressSchemaJOI.ts
@@ -4,8 +4,8 @@ const postAddressSchema = Joi.object().keys({
   zipcode: Joi.string().required(),
   street: Joi.string().required(),
   streetNumber: Joi.string().required(),
-  lat: Joi.string().required(),
-  long: Joi.string().required(),
+  lat: Joi.string(),
+  long: Joi.string(),
   cityId: Joi.string().required(),
   countryId: Joi.string().required(),
 });
