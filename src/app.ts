68abf9cd91864626c9e1e2a0e388506797f399cb
Mathias Ryckewaert <mathryckdel@free.fr>
2021-07-26 11:45:20 +0200

    change the use of cors in app

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,34 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import helmet from "helmet";
 import expressJSDocSwagger from "express-jsdoc-swagger";
 import errorHandler from "./middleware/errorHandler";
 import api from "./api";
 import options from "./swaggerOptions";
-import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 const app = express();
+const whiteList: Array<string> = process.env.ORIGINS!.split(",");
 
 // Basics middlewares needed to build our api
-app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(helmet());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (whiteList.indexOf(origin!) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by cors"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 // Swagger documentation
 
 if (process.env.NODE_ENVV !== "test") {
