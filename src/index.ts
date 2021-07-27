78cd07942cf9ecc5cfa38758f2bdaefa7257f738
Mathias Ryckewaert <mathryckdel@free.fr>
2021-07-27 10:23:30 +0200

    delete an useless async

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, async () => {
+app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on http://localhost:${port}`); //Show the port use
 });
